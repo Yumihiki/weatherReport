1f63e2d449011789c848c875e63c0cfc6ae6b7b5
Yumihiki <nibu10.majik@gmail.com>
2022-05-04 14:13:09 +0900

    初心者の方が見てもわかりやすいようにサンプルの作成

---
+++ env_sample.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+"""LINEBotを扱う上で必要な秘匿情報を設定するSample
+
+本プロジェクトではenv.pyというモジュールは.gitignoreでgit管理されないようになっています。
+本ファイルをenv.pyにリネームしてLINEBotの設定を以下の定数値に入れるようにしてください。
+"""
+
+CHANNEL_ACCESS_TOKEN = ''
+CHANNEL_SECRET = ''
+SEND_USER_ID = ''
+SEND_GROUP_ID = ''
