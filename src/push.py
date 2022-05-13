ac5c608756f7fdb48d2a9bc214ed976825a55116
Yumihiki <nibu10.majik@gmail.com>
2022-05-13 20:00:21 +0900

    mainで何をしているか明示的にした

--- src/push.py
+++ src/push.py
@@ -15,7 +15,7 @@ def push():
     try:
         LINE_BOT_API.push_message(
             env.SEND_USER_ID,
-            TextSendMessage(text=main.main(False))
+            TextSendMessage(text=main.main(is_local_debug=False))
         )
     except LineBotApiError as error:
         LINE_BOT_API.push_message(
