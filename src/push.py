0b019bc7a1c9d7555e1bdeecf301ba0f69abae9a
Yumihiki <nibu10.majik@gmail.com>
2022-05-14 08:56:49 +0900

    必要なものだけimportするように修正

--- src/push.py
+++ src/push.py
@@ -4,22 +4,22 @@ from linebot import LineBotApi
 from linebot.exceptions import LineBotApiError
 from linebot.models import TextSendMessage
 
-import env
+from env import CHANNEL_ACCESS_TOKEN, SEND_USER_ID
 from main import main
 
-LINE_BOT_API = LineBotApi(env.CHANNEL_ACCESS_TOKEN)
+LINE_BOT_API = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 
 def push():
     """ プッシュ通知をする"""
     try:
         LINE_BOT_API.push_message(
-            env.SEND_USER_ID,
+            SEND_USER_ID,
             TextSendMessage(text=main(is_local_debug=False))
         )
     except LineBotApiError as error:
         LINE_BOT_API.push_message(
-            env.SEND_USER_ID,
+            SEND_USER_ID,
             TextSendMessage(text='エラーが発生しました。作成者に問い合わせてください。')
         )
         print(error)
