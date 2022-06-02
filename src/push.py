2f2972525eeb5467b722511d2c62352f26a881c7
Yumihiki <nibu10.majik@gmail.com>
2022-06-02 23:02:26 +0900

    プッシュメッセージの一部置き換え

--- src/push.py
+++ src/push.py
@@ -5,7 +5,7 @@ from linebot.exceptions import LineBotApiError
 from linebot.models import TextSendMessage
 
 from env import CHANNEL_ACCESS_TOKEN, SEND_USER_ID
-from weather_report import weather_report, weather_report_telop, weather_link
+from weather_report import weather_report_telop, weather_link, WeatherReport
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
@@ -13,9 +13,14 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 def push():
     """ プッシュ通知をする"""
     try:
+        wr = WeatherReport(is_local_debug=False)
+        wr.set_weather_data()
+    except Exception:
+        pass
+    try:
         line_bot_api.push_message(
             SEND_USER_ID,
-            [TextSendMessage(text=weather_report(is_local_debug=False)),
+            [TextSendMessage(text=wr.forecasts()),
              TextSendMessage(text=weather_report_telop(is_local_debug=False)),
              TextSendMessage(text=weather_link(is_local_debug=False))]
         )
